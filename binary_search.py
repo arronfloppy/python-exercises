30eb82847ba9b3856d96bcf02ccd8fe5f950675a
aronfloppy <florin2p@gmail.com>
2022-07-18 20:01:29 +0200

    description for binary_search

--- binary_search.py
+++ binary_search.py
@@ -1,8 +1,8 @@
-""" This is a binary search function it searches item in list returns the index or None
-Complexity is O(log n) where n is the length of the list """
+
 
 def binary_search(list, item):
-    
+    """ binary search function it searches item in list returns the index or None
+Complexity is O(log n) where n is the length of the list"""
     start = 0
     end = len(list) -1
     while start <= end:
