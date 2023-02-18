246230c8d1a21b2798bdc54538a8794c87381955
arronfloppy <florin2p@gmail.com>
2023-02-18 18:29:10 +0100

    added knapsack dynamic programming

--- knapsack.py
+++ knapsack.py
@@ -16,22 +16,24 @@ def knapsack_greedy(maxCapacity, items):
         available items as { itemName -> str, (itemWeight -> int, itemValue : int)}
     """
    
-    def find_most_expensive(items, processed):
-         # infinite negative cost
-        inf = float("-inf")
-        mostExpensiveVal = inf
+    def find_most_expensive(items, processed, maxWeight):
+        """ find the most expensive item that fits """
+        # infinite negative cost
+        mininf = float("-inf")
+        mostExpensiveVal = mininf
         mostExpensiveName = None
-        mostExpensiveWeight = inf
+        mostExpensiveWeight = mininf
 
         for item in items.items():
             itemName = item[0]
-            itemValue = item[1][1]
             itemWeight = item[1][0]
+            itemValue = item[1][1]
 
             if(itemName in processed):
                 continue
 
-            if((itemValue > mostExpensiveVal) or (itemValue == mostExpensiveVal and itemWeight < mostExpensiveWeight)):
+            if( ((itemValue > mostExpensiveVal) or (itemValue == mostExpensiveVal and
+             itemWeight < mostExpensiveWeight)) and itemWeight <= maxWeight):
                 mostExpensiveVal = itemValue
                 mostExpensiveName = itemName
                 mostExpensiveWeight = itemWeight
@@ -45,19 +47,95 @@ def knapsack_greedy(maxCapacity, items):
 
     #knapsack content
     itemList = []
+    totalWeight = 0
+    totalValue = 0
 
     availableCapacity = maxCapacity
 
-    (itemName, itemWeight, itemValue) = find_most_expensive(items, processed)
+    (itemName, itemWeight, itemValue) = find_most_expensive(items, processed, availableCapacity)
 
     while itemName:
-        if( itemWeight <= availableCapacity):
-            availableCapacity -= itemWeight
-            itemList.append((itemName, itemWeight, itemValue))
+        availableCapacity -= itemWeight
+        itemList.append(itemName)
+        totalWeight += itemWeight
+        totalValue += itemValue
 
         processed.append(itemName)
-        (itemName, itemWeight, itemValue)= find_most_expensive(items, processed)
+        (itemName, itemWeight, itemValue)= find_most_expensive(items, processed, availableCapacity)
 
-    return itemList
+    return (itemList, totalWeight, totalValue)
+
+    
+def knapsack_dp(maxCapacity, items):
+    """ Given a set of n items numbered from 1 up to n, each with a weight wi and a value vi,
+    along with a maximum weight capacity W maximize ∑(i to n) v*x 
+
+    Using dynamic programming to divide the problem in n independent problems.
+    Parameters
+    ----------
+    maxCapacity : int
+        max knapsack capacity
+    items : dict
+        available items as { itemName -> str, (itemWeight -> int, itemValue : int)}
+    """
+    # we divide the problems in n sub problems
+    # trying to find the max value for each knapsack from 1 to maxCapacity weight
+
+    # the knapsack table (dynamic programing is always using a table)
+    # each row contains the item name
+    # each cell contains knapsack weights from 1 to maxCapacity
+    # you can fill each cell with the item at this row, with the item(s) at the row -1 or both
+    knapsack = {}
+    # the previous row of the table
+    prevName = ""
+
+    for name in items :
+        knapsack[name] = []
+        for weight in range(1,maxCapacity+1) :
+            currentContent = []
+            currentWeight = 0 
+            currentVal = 0 
+            if not prevName:
+                # the first row
+                if items[name][0] <= weight:
+                    currentContent.append(name)
+                    currentWeight = items[name][0]
+                    currentVal = items[name][1]
+                knapsack[name].append((currentContent,currentWeight,currentVal))
+            else: 
+                # the content at row -1
+                prevContent = knapsack[prevName][weight-1][0]
+                prevWeight = knapsack[prevName][weight-1][1]
+                prevVal =  knapsack[prevName][weight-1][2]
+                
+                if items[name][0] > weight:
+                    # cant't fill with current item, using the item at row -1
+                    knapsack[name].append((prevContent,prevWeight,prevVal))
+                else:
+                    # we can fill it with the current item 
+                    # if we have some weight available, find the cell for this weight
+                    # and add the content to current cell
+                    weightAvailable = weight - items[name][0]
+                    currentWeight = items[name][0] + ( knapsack[prevName][weightAvailable-1][1] if weightAvailable else 0)
+                    currentContent.append(name)
+                    if weightAvailable:
+                        currentContent.extend(knapsack[prevName][weightAvailable-1][0])
+                    for elem in currentContent:
+                        currentVal += items[elem][1]
+                    # find the max between the current and the previous value
+                    if(currentVal < prevVal):
+                        knapsack[name].append((prevContent,prevWeight,prevVal))
+                    else:
+                        knapsack[name].append((currentContent,currentWeight,currentVal))
+        prevName = name
+    # if any, the last cell contains the result
+    return knapsack[prevName][-1] if prevName else ([],0,0)
+
+
+            
+
+
+
+
 
     
--- knapsack_test.py
+++ knapsack_test.py
@@ -1,13 +1,21 @@
 from knapsack import *
 
-def test_knapsack_greedy():
-    # using a dict with name = itemName and value =( itemWeight, itemValue)
-    food = {"chips":(1,1), "baguette":(1,2), "salad":(2,2), "burger":(3,2), "hotdog":(4,3), "banana":(6,4) }
+    # using a dict with key = itemName and value =( itemWeight, itemValue)
+food = {"chips":(1,1), "baguette":(1,2), "salad":(2,2), "burger":(3,2),
+     "hotdog":(4,3), "banana":(6,4) }
 
-    result1 = knapsack_greedy(4,food)
-    result2 = knapsack_greedy(5,food)
-    result3 = knapsack_greedy(6,food)
-    result4 = knapsack_greedy(7,food)
+def test_knapsack():
 
-    result = ""
-
+    for weight in range(1, 17):
+        (itemsgr, weightgr, value1gr) = knapsack_greedy(weight,food)
+        assert len(itemsgr) > 0
+        assert weightgr <= weight
+        assert value1gr > 0
+        (itemsdp, weightdp, value1dp) = knapsack_dp(weight,food)
+        assert len(itemsdp) > 0
+        assert weightdp <= weight
+        assert value1dp > 0
+        # dynamic programming version is almost always better than greedy version
+        assert weightdp >= weightgr
+        assert value1dp >= value1gr
+   
