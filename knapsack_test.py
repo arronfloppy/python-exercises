3071fa77c1aeaeed9b01309adeddfbe807bd3238
arronfloppy <florin2p@gmail.com>
2023-02-18 18:53:00 +0100

    minor changes

--- knapsack_test.py
+++ knapsack_test.py
@@ -7,14 +7,14 @@ food = {"chips":(1,1), "baguette":(1,2), "salad":(2,2), "burger":(3,3),
 def test_knapsack():
 
     for weight in range(1, 17):
-        (itemsgr, weightgr, value1gr) = knapsack_greedy(weight,food)
+        (itemsgr, weightgr, valuegr) = knapsack_greedy(weight,food)
         assert len(itemsgr) > 0
         assert weightgr <= weight
-        assert value1gr > 0
-        (itemsdp, weightdp, value1dp) = knapsack_dp(weight,food)
+        assert valuegr > 0
+        (itemsdp, weightdp, valuedp) = knapsack_dp(weight,food)
         assert len(itemsdp) > 0
         assert weightdp <= weight
-        assert value1dp > 0
+        assert valuedp > 0
         # dynamic programming version is almost always better than greedy version
-        assert value1dp >= value1gr
+        assert valuedp >= valuegr
    
